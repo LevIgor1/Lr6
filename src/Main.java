6c658adcb739d37a42eeee1560153d1de263c8ec
sachko65 <levchencow@gmail.com>
2023-11-09 11:03:35 +0200

    test2

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,23 @@ public class Main {
         double avgs = Arrays.stream(arr).average().getAsDouble();
         System.out.println("\nМаксимальне число: " + maxim);
         System.out.println("Середня арифметичне: " + avgs);
+
+
+        double [][] arr2;
+        System.out.print("Введіть кількість елементів: ");
+        n = in.nextInt();
+        arr2 = new double[n][2];
+        for (int i = 0; i < n; i++) {
+            arr2[i][0] = -20 + Math.random() * 50;
+            arr2[i][1] = -20 + Math.random() * 50;
+            System.out.println("x: " + arr2[i][0] + " y: " +  arr2[i][1]);
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr2[i][0] > 0 && arr2[i][1] < 0)
+                count++;
+        }
+        System.out.println("Кількість точок в 4 площіні: " + count);
+
     }
 }
