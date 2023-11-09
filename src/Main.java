dab84da354035783b484c227f77064556dc513bb
sachko65 <levchencow@gmail.com>
2023-11-09 10:49:17 +0200

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        double [] arr;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введіть кількість елементів: ");
+        int n = in.nextInt();
+        arr = new double[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = Math.random() * 5;
+            System.out.print(arr[i] + "\n");
         }
+        double maxim = Arrays.stream(arr).max().getAsDouble();
+        double avgs = Arrays.stream(arr).average().getAsDouble();
+        System.out.println("\nМаксимальне число: " + maxim);
+        System.out.println("Середня арифметичне: " + avgs);
     }
 }
